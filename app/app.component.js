be61a574f8bf2df1709612165ce5a22142ec1b91
Joe Stowers <greenchiletexan@gmail.com>
2017-01-15 22:25:07 -0700

    Try to implment binding component that references the app.component's properties.  No success yet.

--- app/app.component.js
+++ app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', './courses.component', './authors.component'], function(exports_1, context_1) {
+System.register(['angular2/core', './courses.component', './authors.component', './binding.component'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,7 +10,7 @@ System.register(['angular2/core', './courses.component', './authors.component'],
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, courses_component_1, authors_component_1;
+    var core_1, courses_component_1, authors_component_1, binding_component_1;
     var AppComponent;
     return {
         setters:[
@@ -22,43 +22,22 @@ System.register(['angular2/core', './courses.component', './authors.component'],
             },
             function (authors_component_1_1) {
                 authors_component_1 = authors_component_1_1;
+            },
+            function (binding_component_1_1) {
+                binding_component_1 = binding_component_1_1;
             }],
         execute: function() {
             AppComponent = (function () {
                 function AppComponent() {
                     this.title = "My First Angular 2 App";
                     this.imageUrl = "http://lorempixel.com/400/200/";
-                    // Bootstrap button class modifiers
-                    this.isActive = true;
-                    this.isDisabled = true;
                 }
-                AppComponent.prototype.onDivClick = function ($event) {
-                    console.log('Click Event Propagation', $event.target);
-                };
-                ;
-                // Can use methods to get access to the DOM element that 
-                // raised the event.
-                // $event => gives you access to the DOM event (object)
-                AppComponent.prototype.onClick = function ($event) {
-                    $event.stopPropagation();
-                    console.log('Clicked', $event);
-                };
-                ;
-                // implement two-data binding manually
-                AppComponent.prototype.onChangeInput = function ($event) {
-                    this.title = $event.target.value;
-                    console.log('this.title =', this.title);
-                };
-                AppComponent.prototype.onClearTitle = function () {
-                    this.title = '';
-                    console.log('this.title =', this.title);
-                };
                 AppComponent = __decorate([
                     core_1.Component({
                         // when Ang2 sees 'my-app', it will place the <h1> and <courses> elements
                         selector: 'my-app',
-                        template: "\n    \t\t<h1>{{ title }}</h1>\n    \t\t<img [src] = \"imageUrl\" />\n\n    \t\t<input type = \"text\" [value] = \"title\" (input) = \"onChangeInput($event)\">\n\n    \t\t<input type = \"button\"(click)=onClearTitle() value = \"Clear\" />\n\n    \t\t<div (click) = \"onDivClick($event)\">\n    \t\t\t<button\n    \t\t\t\tclass = \"btn btn-lg btn-primary\"\n    \t\t\t\t[style.backgroundColor]= \"isActive ? 'light blue' : 'gray'\"\t\n    \t\t\t\t(click) = 'onClick($event)'>Lg Submit\n\t\t\t\t</button>\n    \t\t</div>\n\t\t\t<button\n\t\t\t\tclass = \"btn btn-md btn-primary\" \n    \t\t\t[class.disabled]=\"isDisabled\">Med Disabled\n\t\t\t</button>\n    \t\t<courses></courses>\n    \t\t<authors></authors>\n    \t\t",
-                        directives: [courses_component_1.CoursesComponent, authors_component_1.AuthorsComponent]
+                        template: "\n    \t\t\t<h1>{{ title }}</h1>\n    \t\t\t<img [src] = \"imageUrl\" />\n    \t\t\t<authors></authors>\n    \t\t\t<binding></binding>\n    \t\t",
+                        directives: [courses_component_1.CoursesComponent, authors_component_1.AuthorsComponent, binding_component_1.BindingComponent]
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -6,72 +6,29 @@ import {CourseService} from './course.service';
 import {AuthorsComponent} from './authors.component';
 import {AuthorService} from './authors.service';
 
+import {BindingComponent} from './binding.component';
+
 @Component({
 	// when Ang2 sees 'my-app', it will place the <h1> and <courses> elements
     selector: 'my-app',
     template: `
-    		<h1>{{ title }}</h1>
-    		<img [src] = "imageUrl" />
-
-    		<input type = "text" [value] = "title" (input) = "onChangeInput($event)">
-
-    		<input type = "button"(click)=onClearTitle() value = "Clear" />
-
-    		<div (click) = "onDivClick($event)">
-    			<button
-    				class = "btn btn-lg btn-primary"
-    				[style.backgroundColor]= "isActive ? 'light blue' : 'gray'"	
-    				(click) = 'onClick($event)'>Lg Submit
-				</button>
-    		</div>
-			<button
-				class = "btn btn-md btn-primary" 
-    			[class.disabled]="isDisabled">Med Disabled
-			</button>
-    		<courses></courses>
-    		<authors></authors>
+    			<h1>{{ title }}</h1>
+    			<img [src] = "imageUrl" />
+    			<authors></authors>
+    			<binding></binding>
     		`,
-    directives: [CoursesComponent, AuthorsComponent]
+
+    directives: [CoursesComponent, AuthorsComponent, BindingComponent]
 })
 
 // view component that takes control over entire app
 // lorempixel.com displays random photos to test your app template
-export class AppComponent { 
+export class AppComponent {
+
 	title = "My First Angular 2 App";
 	
 	imageUrl = "http://lorempixel.com/400/200/";
 	
-	// Bootstrap button class modifiers
-	isActive = true;
-	isDisabled = true;
-
-	
-
-
-	onDivClick($event) {
-		console.log('Click Event Propagation', $event.target);
-	};
-
-	// Can use methods to get access to the DOM element that 
-	// raised the event.
-	// $event => gives you access to the DOM event (object)
-	onClick($event) {
-		$event.stopPropagation();
-		console.log('Clicked', $event)
-	};
-
-	// implement two-data binding manually
-	onChangeInput($event) {
-		this.title = $event.target.value;
-		console.log('this.title =', this.title);
-	}
-
-	onClearTitle() {
-		this.title = '';
-		console.log('this.title =', this.title)
-	}
-
-
 }
 
 /*
